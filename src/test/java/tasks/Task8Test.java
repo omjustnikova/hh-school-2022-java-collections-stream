27694e0ec0df29d61327a0248f846e1b5468902c
Oksana Mjasnikova <omjustnikova@gmail.com>
2022-12-27 18:03:21 +0600

    Extra imports were removed

--- src/test/java/tasks/Task8Test.java
+++ src/test/java/tasks/Task8Test.java
@@ -3,18 +3,12 @@ package tasks;
 import common.Person;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.Instant;
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.when;
 
 public class Task8Test {
 
